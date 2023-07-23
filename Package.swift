eaafc2018a08b94f631d248e6c83a5d56ff4ebf7
erokha <dev@erokha.com>
2023-07-23 06:07:19 +0300

    remove unused staff

--- Package.swift
+++ Package.swift
@@ -4,16 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "EFoundation",
+    name: "Erokha Foundation",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "EFoundation",
-            targets: ["EFoundation"]
-        ),
         .library(
             name: "EFNetwork",
             targets: ["EFNetwork"]
@@ -31,20 +27,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "EFoundation",
-            dependencies: []
-        ),
-        .target(
             name: "EFNetwork",
             dependencies: []
         ),
         .target(
             name: "EFStorage",
             dependencies: []
-        ),
-        .testTarget(
-            name: "EFoundationTests",
-            dependencies: ["EFoundation"]
         ),
         .testTarget(
             name: "EFStorageTests",
