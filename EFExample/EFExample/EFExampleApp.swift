04ad6520733da394d50a44276291b38a0895e541
erokha <dev@erokha.com>
2025-01-31 07:51:31 +0300

    add example app

---
+++ EFExample/EFExample/EFExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFExampleApp.swift
+//  EFExample
+//
+//  Created by Nikita Erokhin on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct EFExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
